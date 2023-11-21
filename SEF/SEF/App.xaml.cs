4005d6acb58a3f210a166f270f91e1cea345e2d7
PepaZDepa21 <honzakovar2110@gmail.com>
2023-11-21 12:46:22 +0100

    Add project files.

---
+++ SEF/SEF/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SEF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                HeightRequest = 80,
+                BarBackgroundColor = Color.FromRgb(46, 35, 32),
+                BackgroundColor = Color.FromRgb(21, 11, 44),
+                BarTextColor = Color.FromHex("#B8CDF8"),
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
